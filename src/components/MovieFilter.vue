7a33c9406a623313aa246f269e7a67c355bc5c7c
Fernando  Crozetta <fernando@czetta.com>
2018-09-10 20:43:51 -0300

    sec15 - class94

---
+++ src/components/MovieFilter.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="movie-filter">
+    <h2>Filter Results</h2>
+    <h3>By Time</h3>
+    <div class="filter-group">
+      <check-filter category="time"
+                    v-bind:key="time.id"
+                    v-for="time in times"
+                    v-bind:title="time"
+                    v-on:check-filter="checkFilter">
+      </check-filter>
+    </div>
+
+    <h3>By Genre</h3>
+    <div class="filter-group">
+      <check-filter category="genre"
+                    v-bind:key="genre.id"
+                    v-for="genre in genres"
+                    v-bind:title="genre"
+                    v-on:check-filter="checkFilter">
+      </check-filter>
+    </div>
+  </div>
+</template>
+
+<script>
+import genres from '../util/genres';
+import times from '../util/times';
+import CheckFilter from './CheckFilter.vue';
+export default {
+  data() {
+    return {
+      genres,
+      times,
+    };
+  },
+  methods: {
+    checkFilter(category, title, check) {
+      this.$emit('check-filter', category, title, check);
+    },
+  },
+  components: {
+    CheckFilter,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,45 @@
+import './style.scss';
+import Vue from 'vue';
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
+Object.defineProperty(Vue.prototype, '$moment', {
+  get() {
+    return this.$root.moment;
+  },
+});
+
+new Vue({
+  el: '#app',
+  data: {
+    genre: [],
+    time: [],
+    movies: [],
+    moment,
+    day: moment(),
+  },
+  methods: {
+    checkFilter(category, title, check) {
+      if (check) {
+        this[category].push(title);
+      } else {
+        let index = this[category].indexOf(title);
+        if (index > -1) {
+          this[category].splice(index, 1);
+        }
+      }
+    },
+  },
+  components: {
+    MovieList,
+    MovieFilter,
+  },
+  created() {
+    this.$http.get('/api').then(response => {
+      this.movies = response.body;
+    });
+  },
+});
